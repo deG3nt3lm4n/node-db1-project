b4532a396ec6308b941ea50d94298cdb9397d464
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-12 21:06:31 -0400

    get all request complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,42 @@
+const Accounts = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+
+  if(!req.body.name || !req.body.budget){
+    res.status(400).json({ message: "name and budget are required" })
+  }else if(typeof(req.params.name) !== 'string'){
+    res.status(400).json({ message: "name of account must be a string" })
+  }else if(!req.body.name.trim().length < 3 || !req.params.body.trim().length > 100 ){
+    res.status(400).json({ message: "name of account must be between 3 and 100" })
+  }else if(typeof(req.params.budget) !== 'number'){
+    res.status(400).json({ message: "budget of account must be a number" })
+  }else if(req.params.budget < 0 || req.params.budget > 1000000){
+    res.status(400).json({ message: "budget of account is too large or too small" })
+  }
+
+  next()
+
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = Accounts.getById(req.params.id)
+
+    if(account){
+      req.account = account
+      next()
+    }else{
+      res.status(404).json({message: 'sorry the account doesnt exist'})
+    }
+
+  } catch (err) {
+    next(err)
+  }
+
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,52 @@
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Accounts.getAll()
+    res.status(200).json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('get by id')
+  } catch (err) {
+    next(err)
+  }
+
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('post')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('put request')
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('delete')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
